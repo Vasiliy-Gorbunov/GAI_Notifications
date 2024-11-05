0ed84a9fc00ef0b53b55d97b9890fa7a753ffac2
Vasiliy-Gorbunov <79896130110@yandex.ru>
2024-11-05 10:16:16 +0300

    initial commit

---
+++ src/main/java/com/gai_app/gai_notifications/GaiNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.gai_app.gai_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaiNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaiNotificationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gai_app/gai_notifications/GaiNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gai_app.gai_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaiNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
